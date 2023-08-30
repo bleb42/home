91d58b68addbe46b1baf744edcb35bd19e0048b2
bleb42 <glebnizamov6@gmail.com>
2023-08-30 16:55:46 +0200

    1

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private float _changingVolumeSpeed;
+
+    private AudioSource _audioSource;
+    private bool _isThiefInside = false;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = 0;
+    }
+
+    private void Update()
+    {
+        if (_isThiefInside == true)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, 1, _changingVolumeSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<ThiefController>())
+        {
+            _audioSource.enabled= true;
+            _isThiefInside = true;
+            _audioSource.Play();
+        }
+    }
+}
